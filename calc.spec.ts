629c71ce7b1ca2d29bf1252943be0bd3ec8bf019
Joel Kurian <joeltkurian@gmail.com>
2021-12-27 10:48:04 -0600

    multiply functionality

--- calc.spec.ts
+++ calc.spec.ts
@@ -1,12 +1,16 @@
-import {add, subtract} from "./calc-functions"
+import {add, multiply, subtract} from "./calc-functions"
 
 describe("calculator tests", ()=>{
     it("Should add 2+2", ()=>{
         const sum= add(2,2);
         expect(sum).toBe(4);
-    })
+    });
     it("Should subtract 4-2", ()=>{
         const diff= subtract(4,2);
         expect(diff).toBe(2);
-    })
+    });
+    it("Should multiply 5*5", ()=>{
+        const val= multiply(5,5);
+        expect(val).toBe(2);
+    });
 })
