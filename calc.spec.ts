b139e5d5479df707f6523b1d24e241d4427dde91
Joel Kurian <joeltkurian@gmail.com>
2021-12-27 10:39:02 -0600

    added subtract functionality to calc-functions

--- calc.spec.ts
+++ calc.spec.ts
@@ -1,8 +1,12 @@
-import {add} from "./calc-functions"
+import {add, subtract} from "./calc-functions"
 
 describe("calculator tests", ()=>{
     it("Should add 2+2", ()=>{
         const sum= add(2,2);
         expect(sum).toBe(4);
+    })
+    it("Should subtract 4-2", ()=>{
+        const diff= subtract(4,2);
+        expect(diff).toBe(2);
     })
 })
